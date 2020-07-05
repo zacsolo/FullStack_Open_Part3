420c6134584b263f4bb7acb85f8c92fe30b59f51
Zac Solomon <zac@zacsolo.com>
2020-07-05 08:56:23 -0400

    frontend and backend heroku update

---
+++ build/precache-manifest.dec039640c5e9d936686130fe69dad75.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0ce782caee31ce8eb3e01f73b09de458",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e5d55a9736810a8a4869",
+    "url": "/static/css/main.3c37dc6c.chunk.css"
+  },
+  {
+    "revision": "c053bb79508f6d6753cb",
+    "url": "/static/js/2.3349295e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.3349295e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e5d55a9736810a8a4869",
+    "url": "/static/js/main.7dd84616.chunk.js"
+  },
+  {
+    "revision": "f4d2ac87a76b6331fd91",
+    "url": "/static/js/runtime-main.4234e0d5.js"
+  }
+]);
